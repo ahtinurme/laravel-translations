6d21ee03eb54b0413f0c0bc353ece1c9c7c6d576
ahtinurme <282119+ahtinurme@users.noreply.github.com>
2025-02-28 09:32:40 +0000

    Fix styling

--- src/Http/Controllers/TranslationController.php
+++ src/Http/Controllers/TranslationController.php
@@ -6,7 +6,6 @@ use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Artisan;
 use Inertia\Inertia;
 use Inertia\Response;
 use Momentum\Modal\Modal;
